b9e0754c338669eb966171eed651f3c298c826a4
jo1vulin <jo1vulin@gmail.com>
2013-11-23 11:10:53 +0100

    [First commit] - Adding and removing last element

---
+++ StackUsingLinkedList/StackUsingLinkedList/Runner.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackUsingLinkedList
+{
+    class Runner
+    {
+        public static void Main(string[] args)
+        {
+            Stack<int> newStack = new Stack<int>();
+            newStack.addElement(1);
+            newStack.addElement(2);
+            newStack.addElement(3);
+            newStack.addElement(4);
+            newStack.addElement(5);
+            newStack.addElement(6);
+
+            Console.WriteLine("ispis elemenata: ");
+
+
+            newStack.ListNodes();
+
+            try
+            {
+                newStack.removeElement();
+                newStack.removeElement();
+                newStack.removeElement();
+                newStack.removeElement();
+                newStack.removeElement();
+                newStack.removeElement();
+                newStack.removeElement();
+                Console.WriteLine("Uklonjen poslednji element. Ispisi ponovo: ");
+                newStack.ListNodes();
+            }
+            catch(InvalidOperationException exc)
+            {
+                Console.WriteLine("Greska: " + exc.Message);
+            }
+            
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ StackUsingLinkedList/StackUsingLinkedList/Stack.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackUsingLinkedList
+{
+    public class Stack<T>
+    {
+        //Clanovi stack-a
+        public class Elements
+        {
+            //clan stack-a tipa T
+            public T Element;
+            //Svaki clan stack-a zna koji mu je prethodni clan(tipa Element) u nizu 
+            public Elements Previous;
+        }
+
+        private int size;
+        public int Count { get; private set; }
+        private Elements firstElement;
+        private Elements currentElement;
+
+        //Dodaj novi clan
+        //ako je prvi clan null onda dodeli vrednost ovog elmenta prvom clanu
+        //ako nije prvi clan, dodaj ovom clanu Previous, trenutni element
+        //na kraju trenutnom elementu dodeli vrednost novog elementa
+        public void addElement(T elementForAdding)
+        {
+            size++;
+            var newElement = new Elements()
+            {
+                Element = elementForAdding
+            };
+
+            if (firstElement == null)
+            {
+                firstElement = newElement;
+            }
+            else
+            {
+                newElement.Previous = currentElement;
+            }
+            currentElement = newElement;
+        }
+
+        //Ukloni poslednji element
+        public void removeElement()
+        {
+            if (size < 1)
+            {
+                throw new InvalidOperationException("Lista je prazna");
+            }
+            else
+            {
+                size--;
+                Elements tempElement = currentElement.Previous;
+                currentElement = tempElement;
+            }
+
+        }
+
+
+        //Test for adding elements to the list
+        public void ListNodes()
+        {
+            Elements tempNode = currentElement;
+
+            while (tempNode != null)
+            {
+                Console.WriteLine(tempNode.Element);
+                tempNode = tempNode.Previous;
+            }
+        }
+    }
+}
